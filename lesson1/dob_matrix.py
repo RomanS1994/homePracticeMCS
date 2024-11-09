5d094bc669ea6d6cfa23f8bf7313e87fda092567
Roman Stryzhka <rstrizhko1994@gmail.com>
2024-11-09 22:52:39 +0100

    add new file

---
+++ lesson1/dob_matrix.py
@@ -0,0 +1,27 @@
+import numpy as np 
+a = np.array([[4, 6], [3, 2]])
+b = np.array([[8, 2], [3, 1]])
+
+task1 = a.dot(b)
+# print(task1)
+
+
+c = np.array([[2, 3], [6, 1]])
+d = np.array([[2, 5], [1, 3]])
+task2 = c.dot(d)
+# print(task2)
+
+f = np.array([[9, 2], [7, 3]])
+e = np.array([[5, 4], [2, 1]])
+task3 = f.dot(e)
+# print(task3)
+
+
+
+r = np.array([ [1, 2], [3, 4], [5,6]])
+t = np.array([ [7, 8, 9], [10, 11, 12] ])
+task4 = r.dot(t)
+print(task4)
+
+
+
---
+++ lesson1/test.py
@@ -0,0 +1,15 @@
+import numpy as np
+a = np.array([ [1, 1], [2, 2], [3, 3], [4, 4], [5,5]]
+
+
+
+
+
+
+
+
+b = np.array([ [1,1,1], [2,2,2] ])
+print(a)
+print(b)
+total = a.dot(b)
+print(total)
