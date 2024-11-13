d968382805859e57600d887cdb2deeb1e29a7782
Roman Stryzhka <rstrizhko1994@gmail.com>
2024-11-13 21:57:31 +0100

    update file hw.py

--- lesson1/home_work.py
+++ lesson1/home_work.py
@@ -1,6 +1,16 @@
+import cv2 as cv
+import urllib
 import numpy as np
-# Код оголошення векторів не можна змінювати
-a = np.array([[1, 2, 3, 4, 5]])
-b = np.array([[1/2, 1, 2, 3, 4]])
+from google.colab.patches import cv2_imshow as cv_imshow
 
-print(a / b)
+# Read an image
+def read_image_by_url(url):
+    req = urllib.request.urlopen(url)
+    arr = np.asarray(bytearray(req.read()), dtype=np.uint8)
+    img = cv.imdecode(arr, -1)
+    return img
+
+url = 'https://gptchat.in.ua/wp-content/uploads/2023/02/iryna_kunichenko_many_colored_balloons_over_the_grand_canyon_hd_06263781-130a-4137-a4c8-2e7454b401c7.jpg'
+
+img = read_image_by_url(url)
+print(img)
